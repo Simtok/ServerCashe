e77a0f3f148fa8bf799b0cc9ed8f1f44ba5c1b5a
Simtok <yurastov@gmail.com>
2021-03-10 16:15:25 +0300

    change config

--- src/config/config.js
+++ src/config/config.js
@@ -19,4 +19,14 @@ module.exports = {
       host: 'localhost',
     },
   },
+  productions: {
+    database: 'd16380rb1c3veq',
+    username: 'wotedxuggsilhn',
+    password: '476a1d4b2f1e087f1e06382eed842ed253601be1bb8dab4c7a443cfffb3a336c',
+    options: {
+      host: 'ec2-3-232-163-23.compute-1.amazonaws.com',
+      dialect: 'postgres',
+      port: 5432,
+    },
+  },
 }
